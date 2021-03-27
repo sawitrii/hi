c16a3a5d7bb43e7c2d366062db2e127beacc62cd
sawitrii <65085738+sawitrii@users.noreply.github.com>
2021-03-27 17:59:29 +0100

    Add files via upload

---
+++ hi/index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.login('ODIyMTcwNzM5OTkzNzM5Mjg1.YFOYKw.DzmvayZtVvoE2ttUdI41xzavKvg');
